c23d646fcdc0b45dad968e05239a4b15b683b4cb
Patrick Goli <patrick.goli@ingroupe.com>
2023-01-04 18:12:33 +0100

    fix: this.state undefined

--- pages/campaign/new.js
+++ pages/campaign/new.js
@@ -8,13 +8,14 @@ class NewCampaign extends Component {
     constructor(props) {
         super(props);
         this.state = {minContribution: 0};
+        this.onSubmit = this.onSubmit.bind(this);
+
       }
 
     async onSubmit(event) {
         event.preventDefault();
         const accounts = await web3.eth.getAccounts();
         //create new campaign
-        console.log(this)
         await factory.methods.createCampaign(this.state.minContribution).send({
             from: accounts[0]
         });

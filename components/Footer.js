ad9f7dde33372dbe9f1539459b6d518d199df6e3
Patrick Goli <patrick.goli@ingroupe.com>
2023-01-09 18:21:08 +0100

    added error handling in campaign creation

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
  
 const Footer = () => {
-    return <footer style={{marginTop: '50px'}}>FOOTER HERE</footer>
+    return <footer style={{marginTop: '100px'}}>FOOTER HERE</footer>
 };
 export default Footer;
--- pages/campaign/new.js
+++ pages/campaign/new.js
@@ -7,18 +7,25 @@ import web3 from "../../ethereum/web3";
 class NewCampaign extends Component {
     constructor(props) {
         super(props);
-        this.state = {minContribution: 0};
+        this.state = {minContribution: 0, message: ''};
         this.onSubmit = this.onSubmit.bind(this);
 
       }
 
     async onSubmit(event) {
         event.preventDefault();
-        const accounts = await web3.eth.getAccounts();
-        //create new campaign
-        await factory.methods.createCampaign(this.state.minContribution).send({
-            from: accounts[0]
-        });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            //create new campaign
+            await factory.methods.createCampaign(this.state.minContribution).send({
+                from: accounts[0]
+            });
+            this.setState({message: 'Campaign creatiion submited'})
+        } catch (error) {
+            console.log(error);
+            this.setState({message: 'Campaign creatiion failed: ' + error?.message})
+        }
+
     }
 
     render(){
@@ -37,6 +44,8 @@ class NewCampaign extends Component {
                 />
             </Form.Field>
         <Button type='submit' primary>Create</Button>
+        <br></br>
+        <label><strong>{this.state.message}</strong></label>
         </Form>
         </Container>
     }

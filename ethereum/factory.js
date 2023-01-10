0bb995b0cf9b89464e082165bed67b51c43dc5d9
Patrick Goli <patrick.goli@ingroupe.com>
2023-01-10 15:50:37 +0100

    new contract version

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,5 +1,5 @@
 import web3 from "./web3";
-const address = "0x17e280fbc12E930D9E4c905D9C38b802348B2186";
+const address = "0x74e8D8D7c18Ff970B7ada4B23715bc140D1bc403";
 const compiledFactory = require("./build/CampaignFactory.json");
 
 export default new web3.eth.Contract(JSON.parse(compiledFactory.interface), address);
